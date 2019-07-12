29e49ca4319a37f17bffc8104e15d4131d236736
Qian <eli_home@outlook.com>
2019-07-12 19:05:29 -0400

    Qian checked

--- 5cc9c2f4e0becab92b6f851032e11261c3f58848/js/summer.js
+++ 5cc9c2f4e0becab92b6f851032e11261c3f58848/js/summer.js
@@ -1,7 +1,23 @@
-$(".button-solid").attr("data-aos", "fade-up");
+function changeTab(buttonID, tabID){
+    // button
+    var active_button = document.getElementsByClassName('tab-navbutton active');
+    if (active_button.length == 1){
+        active_button[0].classList.remove('active');
+    }
+    var target_button = document.getElementById(buttonID);
+    target_button.classList.add('active');
 
-$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-    $(".button-collapse").sideNav();
-});
+    // tab
+    var active_tab = document.getElementsByClassName('tab-page active');
+    if (active_tab.length == 1){
+        active_tab[0].classList.remove('active');
+    }
+    var target_tab = document.getElementById(tabID);
+    target_tab.classList.add('active');
+}
 
+function showDivs(elem, n) {
+    let slideshow = $(elem).parents('.slideshow-wrapper');
+    slideshow.children('.active').removeClass('active');
+    slideshow.children('.slideshow-item').eq(n).addClass('active');
+}
