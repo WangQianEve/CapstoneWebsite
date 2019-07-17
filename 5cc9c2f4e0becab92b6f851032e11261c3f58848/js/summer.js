accce75ba074b606087534652316de8c1b752c87
Unknown <astrid2242@gmail.com>
2019-07-17 03:27:34 -0400

    Image Expand

--- 5cc9c2f4e0becab92b6f851032e11261c3f58848/js/summer.js
+++ 5cc9c2f4e0becab92b6f851032e11261c3f58848/js/summer.js
@@ -10,3 +10,28 @@ function showDivs(elem, n) {
     slideshow.children('.active').removeClass('active');
     slideshow.children('.slideshow-item').eq(n).addClass('active');
 }
+
+
+function setup_expandable_img() {
+    var i;
+    var wrapper = document.getElementById("expanded-img-wrapper");
+    var expanded_img = document.getElementById("expanded-img");
+    var slideshow_list = document.getElementsByClassName('slideshow-img');
+    for (i = 0; i < slideshow_list.length; i++){
+        slideshow_list[i].onclick = function(){
+            wrapper.style.display = 'flex';
+            expanded_img.src = this.src;
+        }
+    }
+    var tab_cover_list = document.getElementsByClassName('tab-cover-image');
+    for (i = 0; i < tab_cover_list.length; i++){
+        tab_cover_list[i].onclick = function(){
+            wrapper.style.display = 'flex';
+            expanded_img.src = this.src;
+        }
+    }
+
+    wrapper.onclick = function() {
+        wrapper.style.display = "none";
+    }
+}
